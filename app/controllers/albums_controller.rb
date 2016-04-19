adf4e079301d99e21620b7fdbb773055bc473ee6
portmare <portmare@gmail.com>
2016-04-19 23:32:27 +0300

    Changed albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,24 +1,49 @@
+# /app/controllers/albums_controller.rb
 class AlbumsController < ApplicationController
-  before_filter :require_user
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+  before_action :set_album, only: [:edit, :update, :destroy]
 
   def index
-  end
-
-  def show
+    authorize Album
+    @albums = policy_scope(Album)
   end
 
   def new
+    authorize Album
+    @album = Album.new
   end
 
   def create
+    authorize Album
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to root_path, notice: 'Album created successfully'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @album.update(album_params)
+      redirect_to root_path, notice: 'Album updated successfully'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @album.destroy
+    redirect_to root_path, notice: 'Album deleted successfully'
+  end
+
+  private
+
+  def set_album
+    @album = Album.find(params[:id])
+    authorize @album
   end
 end
