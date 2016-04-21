20af772ae5bf83b51e45f43ea6326ba610944287
portmare <portmare@gmail.com>
2016-04-21 13:22:56 +0300

    Added tests for view of media

---
+++ spec/views/media/new.html.slim_spec.rb
@@ -0,0 +1,24 @@
+# /spec/views/media/new.html.slim_spec.rb
+require 'rails_helper'
+
+describe 'media/new' do
+  it 'displays text input for link' do
+    render
+    expect(rendered).to have_field('media_link')
+  end
+
+  it 'displays file field for image' do
+    render
+    expect(rendered).to have_field('media_image')
+  end
+
+  it 'displays button #Upload' do
+    render
+    expect(rendered).to have_button('Upload')
+  end
+
+  it 'displays title #Add new image' do
+    render
+    expect(rendered).to have_content('Add new image')
+  end
+end
