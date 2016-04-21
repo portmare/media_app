61189d50a192abd147cab36d38ff31c149d0ec0b
portmare <portmare@gmail.com>
2016-04-21 08:48:57 +0300

    Added a capacity to upload image from remote link

--- app/models/media.rb
+++ app/models/media.rb
@@ -2,6 +2,11 @@ class Media < ActiveRecord::Base
   belongs_to :album
 
   validates :album, :image, presence: true
+  before_validation :upload_remote_image, on: :create
 
   mount_uploader :image, ImageUploader
+
+  def upload_remote_image
+    self.remote_image_url = self.link if self.link.present?
+  end
 end
--- spec/models/media_spec.rb
+++ spec/models/media_spec.rb
@@ -16,4 +16,11 @@ describe Media, type: :model do
     image = build(:media, album: nil)
     expect(image).to be_invalid
   end
+
+  it 'upload image from remote server if link not nil' do
+    external = 'http://www.google.com/images/errors/robot.png'
+    media = Media.create(album: build_stubbed(:album), link: external)
+    expect(media).to be_valid
+    expect(media.image.identifier).to eq('robot.png')
+  end
 end
